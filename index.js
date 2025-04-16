3d6b6b3abfc62a66a4adddc80924e66f3155136c
Tevin <38287265+saphby@users.noreply.github.com>
2025-04-16 19:44:43 +0300

    Add Round Logic

--- index.js
+++ index.js
@@ -16,3 +16,45 @@ function getHumanChoice() {
 
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanSelection.toLowerCase();
+   if ((humanChoice == `rock`) && (computerChoice == `Scissors`)) {
+       humanScore++
+       console.log(`You Win! Rock Obliterates Scissors`)
+   } else if ((humanChoice == `scissors`) && (computerChoice == `Rock`)) {
+       computerScore ++
+       console.log(`You Lose! Rock Obliterates Scissors`)
+   }
+   
+   if ((humanChoice == `paper`) && (computerChoice == `Rock`)) {
+       humanScore++
+       console.log(`You Win! Paper literally suffocates Rock`)
+   } else if ((humanChoice == `rock`) && (computerChoice == `Paper`)) {
+       computerScore ++
+       console.log(`You Lose! Paper literally suffocates Rock`)
+   }
+   
+   if ((humanChoice == `scissors`) && (computerChoice == `Paper`)) {
+       humanScore++
+       console.log(`You Win! Scissors slices Paper mercilessly`)
+   } else if ((humanChoice == `paper`) && (computerChoice == `Scissors`)) {
+       computerScore ++
+       console.log(`You Lose! Scissors slices Paper mercilessly`)
+   }
+   checkDraw();
+   }
+   
+   function checkDraw() {
+       getHumanChoice = humanSelection.toLowerCase();
+       getComputerChoice = computerSelection.toLowerCase();
+       if (getHumanChoice == getComputerChoice) {
+           console.log(`It is a draw, you suck`)
+       }
+   }
+   
+   const humanSelection = getHumanChoice()
+   const computerSelection = getComputerChoice()
+   
+   playRound(humanSelection, computerSelection)
+   
